50c1e5ed43c11d34992c7107363e97350e9b302e
Atamai <tuiolosega@husky.neu.edu>
2018-02-28 13:07:53 -0500

    Added BillSplit Activities, Adapter, Model

---
+++ app/src/main/java/com/is4300/homez/activity/billsplit/BillSplitAdapter.java
@@ -0,0 +1,25 @@
+package com.is4300.homez.activity.billsplit;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by tuiolosega.a on 2/28/18.
+ */
+
+public class BillSplitAdapter extends FragmentPagerAdapter {
+
+    public BillSplitAdapter(FragmentManager fm) {
+        super(fm);
+    }
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
