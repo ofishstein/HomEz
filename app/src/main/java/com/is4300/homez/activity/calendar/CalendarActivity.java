16adc046be3c5ed0f49e808e11b6aa75f80565e6
Isabel Tripp <stagestruck1@gmail.com>
2018-02-28 20:10:28 -0500

    minor changes

--- app/src/main/java/com/is4300/homez/activity/calendar/CalendarActivity.java
+++ app/src/main/java/com/is4300/homez/activity/calendar/CalendarActivity.java
@@ -5,7 +5,13 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
+import com.is4300.homez.HomEzApp;
 import com.is4300.homez.R;
+import com.is4300.homez.activity.adapters.DashEventArrayAdapter;
+import com.is4300.homez.model.EventMock;
+import com.is4300.homez.util.UiUtils;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,9 +31,13 @@ public class CalendarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_calendar);
         ButterKnife.bind(this);
 
+        //setUpEventList();
+
         this.calPagerAdapter = new CalendarPagerAdapter(getSupportFragmentManager());
         this.calViewPager.setAdapter(calPagerAdapter);
         this.calViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(calTabLayout));
         this.calTabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(calViewPager));
     }
+
+
 }
