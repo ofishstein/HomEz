385972d4d5c31c59926b3867c06cd00e79fc024b
Ian Leonard <ian.leonard44@gmail.com>
2018-02-28 23:32:13 -0500

    added add_chore activity (was coded blind, will probably have to debug once visible)

--- app/src/main/java/com/is4300/homez/activity/chore/AddChoreActivity.java
+++ app/src/main/java/com/is4300/homez/activity/chore/AddChoreActivity.java
@@ -5,15 +5,33 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.is4300.homez.R;
 
+import butterknife.ButterKnife;
+
 /**
  * Created by ianleonard on 2/28/18.
  */
 
 public class AddChoreActivity extends AppCompatActivity {
 
+    //@BindView(R.id.addButton)
+    //Button addButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_chore);
+        ButterKnife.bind(this);
+        //setOnClickListener();
     }
+/*
+    private void setOnClickListener(){
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+    */
 }
