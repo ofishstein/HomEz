431969e734663edb9f39a84fa3745b3c25a63f7b
Atamai <tuiolosega.a@husky.neu.edu>
2018-03-01 16:40:38 -0500

    Edited BillSplitActivity

--- app/src/main/java/com/is4300/homez/activity/billsplit/BillSplitActivity.java
+++ app/src/main/java/com/is4300/homez/activity/billsplit/BillSplitActivity.java
@@ -1,17 +1,49 @@
 package com.is4300.homez.activity.billsplit;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.is4300.homez.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class BillSplitActivity extends AppCompatActivity {
 
+   // @BindView(R.id.billSplitModule)
+    //ViewPager billSplitView;
+    @BindView(R.id.billSplitModule)
+    TabLayout choreTabLayout;
+
+
     private BillSplitAdapter BillSplitAdapter;
+
+    String tabMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bill_split);
+        ButterKnife.bind(this);
+
+        choreTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                // this should be a class variable
+                tabMode = (String) tab.getText();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                //LEAVE BLANK
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                //LEAVE BLANK
+            }
+        });
     }
 }
