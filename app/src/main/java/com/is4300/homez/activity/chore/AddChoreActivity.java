79b0a59472bd151a566ecd5c65ec83f070b0289b
Oli Fishstein <ofishstein@gmail.com>
2018-04-03 10:55:37 -0400

    make addchore assignee spinner work and fix styling

--- app/src/main/java/com/is4300/homez/activity/chore/AddChoreActivity.java
+++ app/src/main/java/com/is4300/homez/activity/chore/AddChoreActivity.java
@@ -3,7 +3,11 @@ package com.is4300.homez.activity.chore;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.is4300.homez.R;
 
@@ -16,8 +20,13 @@ import butterknife.ButterKnife;
 
 public class AddChoreActivity extends AppCompatActivity {
 
+    String[] spinnerNames={"Ian Leonard","Atamai Tuiolosega","Connor Rouan","Oli Fishstein","Izzi Tripp"};
+
+
     @BindView(R.id.saveButton)
     Button saveButton;
+    @BindView(R.id.assigneeSpinner)
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +34,7 @@ public class AddChoreActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_chore);
         ButterKnife.bind(this);
         setOnClickListener();
+        setUpSpinner();
     }
 
     private void setOnClickListener(){
@@ -32,6 +42,25 @@ public class AddChoreActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 onBackPressed();
+            }
+        });
+    }
+
+    private void setUpSpinner() {
+        //Creating the ArrayAdapter instance having the bank name list
+        ArrayAdapter arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, spinnerNames);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        spinner.setAdapter(arrayAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), spinnerNames[position], Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
             }
         });
     }
